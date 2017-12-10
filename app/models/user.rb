f0a3a9c997eb5f339b2d227348e337a67c66aa04
Oliver <oc@hejverden.dk>
2017-12-10 13:26:26 +0100

    Finish toy app

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :microposts
+  validates :name, length: { maximum: 30 },
+                   presence: true
+
+  validates :email, length: { maximum: 40 },
+                    presence: true
+end
